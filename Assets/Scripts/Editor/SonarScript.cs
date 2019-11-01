60f5c5fa803541f3793fec235a143983dc4adf87
Muhammad Yoga Nugraha Putra <m.yoga@agate.id>
2019-11-01 09:46:32 +0700

    add sonar

---
+++ Assets/Scripts/Editor/SonarScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Agate.Sonarqube
+{
+    public static class SonarScript
+    {
+        public static void GenerateSln()
+        {
+            EditorApplication.ExecuteMenuItem("Assets/Open C# Project");
+        }
+    }
+}
